1478c74758fc78ad426292b6deea85c047fc1b46
murilohenzo <murilohezo@gmail.com>
2024-04-15 11:48:15 -0300

    feat(PET-ADOPTION-FLOW): art: WIP

--- src/main/java/com/murilohenzo/petapi/adapters/mapper/PetMapper.java
+++ src/main/java/com/murilohenzo/petapi/adapters/mapper/PetMapper.java
@@ -6,13 +6,19 @@ import com.murilohenzo.petapi.domain.models.enums.PetStatus;
 import com.murilohenzo.petapi.presentation.representation.PetRequestRepresentation;
 import com.murilohenzo.petapi.presentation.representation.PetResponseRepresentation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface PetMapper {
 
   // PetRepresentation
+  @Mapping(source = "gender", target = "petGender")
+  @Mapping(source = "species", target = "petSpecies")
   public PetDomain petRequestRepresentationToPetDomain(PetRequestRepresentation petRepresentation);
 
+  @Mapping(source = "petStatus", target = "status")
+  @Mapping(source = "petGender", target = "gender")
+  @Mapping(source = "petSpecies", target = "species")
   public PetResponseRepresentation petDomainToPetResponseRepresentation(PetDomain pet);
 
   public PetStatus toStatus(String status);
