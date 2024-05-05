83e5818926f7ca6117879a631f442ef8366df20c
murilohenzo <murilohezo@gmail.com>
2024-05-04 21:32:02 -0300

    WIP

--- ms-pet-api/src/main/java/com/murilohenzo/petapi/domain/ports/StoragePersistencePort.java
+++ ms-pet-api/src/main/java/com/murilohenzo/petapi/domain/ports/StoragePersistencePort.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 public interface StoragePersistencePort {
   
   public String saveImage(String fileName, InputStream is) throws IOException;
-  public byte[] getImage(String imgReferenceId);
-  public boolean deleteImage(String imgReferenceId);
+  public byte[] getImage(String referenceKey);
+  public void deleteImage(String referenceKey);
   
 }
